357c33c6364a34b46a72d7a321d24c796b2e3c73
Angri <Domenico.Angri@Vodafone.com>
2020-02-21 14:22:45 +0100

    Initial configuration for hyconique site.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+        <div className="bkg-img">
+            <div className="container">
+                <div className="top-container">
+                    <h3>H Y C O N I Q U E</h3>
+                </div>
+                
+                <div className="middle-container">
+                    <h2>Coming Soon</h2>
+                </div>
+                
+                <div className="bottom-container">
+                    <p>FOLLOW US</p>
+
+                    <div className="icon-container">
+                        <a className="icon-style" href="http://fb.com/hyconique">
+                            <i className="fab fa-facebook-f"></i>
+                            <span className="icon-footer">fb.com/hyconique</span>
+                        </a>
+
+                        <a className="icon-style" href="https://www.instagram.com/hyconique/">
+                            <i className="fab fa-instagram"></i>
+                            <span className="icon-footer">@hyconique</span>
+                        </a>
+                    </div>
+
+                    <p>www.hyconique.com</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
